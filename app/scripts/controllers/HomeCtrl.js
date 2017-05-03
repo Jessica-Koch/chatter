9ac5b00451b7e8da6c3ae67d44d04dcd85e5b93f
Jessica-Koch <jessicakoch136@icloud.com>
2017-05-02 20:55:11 -0700

    fix scope error

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,23 +1,19 @@
 (function () {
     function HomeCtrl ($scope, Room, Message) {
         this.room = Room;
-
         $scope.rooms = this.room.all;
 
-        // this.messages = this.ssage.getRoomById;
+        // $scope.getByRoomId = Message.getByRoomId;
+        $scope.getByRoomId = function(roomId){
+            return $scope.messages = Message.getByRoomId;
+        }
 
         $scope.activeChatroom = null;
 
         // returns active chatroom
         $scope.activate = function () {
           $scope.activeChatroom = this.rm;
-          console.log(Message.getRoomById($scope.activeChatroom))
-
-            // console.log(Message.getByRoomId($scope.activeChatroom))
-        }
-
-        $scope.messages = function () {
-            return Message.getByRoomId($scope.activeChatroom);
+          $scope.getByRoomId($scope.activeChatroom.$id);
         }
 
     }
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -2,11 +2,10 @@
   function Message($firebaseArray) {
     var Message = {};
     var ref = firebase.database().ref().child("messages");
-    var messages = $firebaseArray(ref);
 
     Message.getByRoomId = function(roomId) {
         // Filter the messages by their room ID.
-        return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+         $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
 
     };
 
